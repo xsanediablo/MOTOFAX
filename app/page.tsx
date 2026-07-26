7bfd87bdd81bf3ebebf6f34d10497f5adb68feb9
xsanediablo <xsanediablo@gmail.com>
2026-07-25 21:01:13 -0400

    Add home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">MOTOFAX</h1>
+      <p className="text-xl mt-4">Welcome to MOTOFAX Web</p>
+    </main>
+  );
+}
